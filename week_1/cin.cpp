cc7fbac6ddde0b4c388bf61d72d87b942a2ec043
Onon064 <99935261+Onon064@users.noreply.github.com>
2022-02-18 11:04:33 +0800

    Create cin.cpp

---
+++ week_1/cin.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    string Age;
+    cout << "what is your name? ";
+    cin >> name;
+
+    cout << "Hello " << name;
+
+    cout << "\n" << "How old are you? ";
+    cin >> Age;
+
+    cout << Age;
+
+    return 0;
+
+
+
+
+}
