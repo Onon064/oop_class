49e09e2befb86683c37cceb0fc8ed68387addd7d
Onon064 <99935261+Onon064@users.noreply.github.com>
2022-02-18 10:49:04 +0800

    Create types.cpp

---
+++ week_1/types.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+
+    // integer
+    int a = 7;
+    cout << a << "\n";
+
+
+    // double
+    double b = 7.007807;
+    cout << b <<"\n";
+
+    //strings
+    string c = "Hello";
+    cout << c <<"\n";
+
+    //boolean
+    bool d = true;
+    cout << d <<"\n";
+
+    return 0;
+}
